c8671b2d445508ca4469d0dca07d7918ad7dd8a2
fatimazaarate <fatima.zaarate@gmail.com>
2024-07-02 21:46:04 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,56 +1,38 @@
 #!/usr/bin/python3
-"""0-nqueens.py"""
-import sys
+"""
+N-Queens Solver
+"""
 
+import sys
 
 def is_safe(board, row, col, n):
     """
-    is_safe func to check if there is a queen in the same column
+    Checks if placing a queen at (row, col) is safe.
     """
     for i in range(row):
-        if board[i][col] == 1:
+        if board[i][col] == 1 or \
+           board[i][col - row + i] == 1 or \
+           board[i][col + row - i] == 1:
             return False
-
-    i = row
-    j = col
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
-
-    i = row
-    j = col
-    while i >= 0 and j < n:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j += 1
-
     return True
 
-
-def solve(board, row, n):
+def find_all_solutions(board, row, n):
     """
-    print the solution
+    Finds all solutions for the N-Queens problem.
     """
     if row == n:
-        print([[i, j] for i in range(n) for j in range(n) if board[i][j] == 1])
-        return True
+        return [[(i, j) for j in range(n) if board[i][j] == 1] for i in range(n)]
 
+    solutions = []
     for col in range(n):
         if is_safe(board, row, col, n):
             board[row][col] = 1
-            solve(board, row + 1, n)
+            solutions.extend(find_all_solutions(board, row + 1, n))
             board[row][col] = 0
 
-    return False
-
+    return solutions
 
 if __name__ == '__main__':
-    """
-    main
-    """
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
@@ -65,8 +47,8 @@ if __name__ == '__main__':
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0 for j in range(n)] for i in range(n)]
-    """
-    running this recursively
-    """
-    solve(board, 0, n)
+    chessboard = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = find_all_solutions(chessboard, 0, n)
+
+    for solution in solutions:
+        print(solution)
